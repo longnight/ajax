3898c0bcc9df55b446e3731a5985f08e7fca3b93
longnight <longnight@gmail.com>
2015-12-29 01:25:39 +0800

    get array

--- ajax1/static/js/ajax1.js
+++ ajax1/static/js/ajax1.js
@@ -1,4 +1,29 @@
-$(".title").click(
-    function(){
-        console.log('yeah good');
-        })
+$(document).ready(function() {
+
+    console.log('so far so good');
+
+    $('#comment_1').click(function(){
+        $('body').css("background", "#bbffaa");
+    })
+
+    $('#comment_2').click(function(){
+        $('body').css("background", "lightblue");
+    })
+
+    console.log($('.comment').text());
+
+    // var $objlist = $('.comment');
+    // console.log($objlist[2];
+    // for (var i=1; i<$objlist.length; i++){
+        // console.log(typeof $objlist[i])
+        // console.log($objlist[i].html());
+
+    // }
+
+
+});
+
+
+
+
+
